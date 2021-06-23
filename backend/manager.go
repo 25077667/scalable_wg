7b4068348b6a6878c6017e781cb3010f3d3bc72e
25077667 <zxc25077667@gmail.com>
2021-06-23 09:28:06 +0800

    Try frontend & backend

--- backend/manager.go
+++ backend/manager.go
@@ -1,13 +1,62 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// Database need to implemented
+var Database = make(map[string]string)
+
+func init_db() {
+	for i := 0; i < 5000; i++ {
+		// user1:amazingvpn
+		sha_bytes := sha256.Sum256([]byte("amazingvpn"))
+		Database["user"+strconv.Itoa(i)] = hex.EncodeToString(sha_bytes[:])
+	}
+}
+
+func get_acc_pw(data []string) (string, string) {
+	account, pw := strings.Split(data[0], "="), strings.Split(data[1], "=")
+	return account[1], pw[1]
+}
+
+func select_server() (string, string) {
+	num, _ := strconv.Atoi(os.Getenv("REGNUM"))
+	var result string
+	for i := 0; i < num; i++ {
+		result = os.Getenv("REMOTE" + strconv.Itoa(i))
+	}
+	// ! FIXME: Token
+	return result, "TOKEN"
+}
 
 func main() {
-    app := fiber.New()
+	init_db()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app := fiber.New()
 
-    app.Listen(":3000")
+	app.Post("/Login", func(c *fiber.Ctx) error {
+		user_data := strings.Split(string(c.Body()), "&")
+		if len(user_data) != 2 {
+			return c.SendString("Invalid input!")
+		}
+		account, raw_pw := get_acc_pw(user_data)
+		sha_bytes := sha256.Sum256([]byte(raw_pw))
+		hashed_pw := hex.EncodeToString(sha_bytes[:])
+
+		if Database[account] == hashed_pw {
+			ip, tok := select_server()
+			return c.SendString(ip + "\n" + tok)
+		} else {
+			return c.SendString("Permission denied!")
+		}
+	})
+
+	app.Listen(":3000")
 }
--- build_essential.sh
+++ build_essential.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# update
+apt update && apt upgrade -y
+
+# essential pkg
+apt install build-essentail nginx git openssh
+
+# install golang
 wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 
+# golang lis
+go get -u github.com/gofiber/fiber/v2
